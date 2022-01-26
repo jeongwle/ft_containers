a86430d485e594069e2bfa040db97f9c6bbf0675
Jeongwook Lee <jeongwle@c5r2s3.42seoul.kr>
2022-01-26 19:05:19 +0900

    [Modified] : 벡터 생성자와 소멸자 틀만 갖추어 놓았다.

--- Vector.hpp
+++ Vector.hpp
@@ -13,19 +13,21 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 #include <memory>
+#include "VectorIterator.hpp"
 
 namespace ft {
     template<class T, class Alloc = std::allocator<T> >
     class vector {
     public :
         /* type def */
-        typedef T                   value_type;
-        typedef Alloc               allocator_type;
-        typedef value_type&         reference;
-        typedef const value_type&   const_reference;
-        typedef value_type*         pointer;
-        typedef const value_type*   const_pointer;
-        typedef size_t              size_type;
+        typedef T                           value_type;
+        typedef Alloc                       allocator_type;
+        typedef value_type&                 reference;
+        typedef const value_type&           const_reference;
+        typedef value_type*                 pointer;
+        typedef const value_type*           const_pointer;
+        typedef size_t                      size_type;
+        typedef VectorIterator<value_type>  iterator;
 
     private :
         /* private variables */
@@ -33,6 +35,29 @@ namespace ft {
         size_type       _capacity;
         size_type       _size;
         pointer         _storage;
+
+    public :
+        /* Constructor, Destructor */
+        explicit vector(const allocator_type& alloc = allocator_type()) : _alloc(alloc), _capacity(0), _size(0), _storage(NULL){
+            /* empty container constructor(default constructor) */
+        }
+        
+        explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()){
+            /* fill constructor : Constructs a container witn n elements. Each element is a copy of val */
+        }
+
+        template<class InputIterator>
+        vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){
+            /* range constructor : Constructs a container with as many elements as the range(first, last) */
+        }
+
+        vector(const vector& copy){
+
+        }
+
+        ~vector(){
+
+        }
     };
 }
 
