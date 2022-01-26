d63c320b965700aee8efa8b055e34ffbbda52310
Jeongwook Lee <jeongwle@c5r2s3.42seoul.kr>
2022-01-26 19:05:41 +0900

    just test 아무의미없음

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 int main(void){
     std::vector<int> intVec;
     intVec.push_back(1);
-    intVec.push_back(1);
-    intVec.push_back(1);
-    intVec.push_back(1);
-    intVec.push_back(1);
-    std::cout << intVec.capacity() << std::endl;
-    std::cout << intVec.size() << std::endl;
-    std::vector<int>::iterator it;
+    intVec.push_back(2);
+    intVec.push_back(3);
+    intVec.push_back(4);
+    intVec.push_back(5);
+    // std::cout << intVec.capacity() << std::endl;
+    // std::cout << intVec.size() << std::endl;
+    std::vector<int>::iterator it1;
+    std::vector<int>::iterator it2;
+    std::vector<int>::iterator it3;
+    it1 = intVec.begin() + 1;
+    it2 = intVec.end() - 1;
+    std::cout << it1[-1] << std::endl;
 }
