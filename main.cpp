b5e2a1e758f2249740b7bc2f80e6a2846b8c87ef
Jeongwook Lee <jeongwle@c5r2s3.42seoul.kr>
2022-02-04 17:55:30 +0900

    just practice for make vector

--- main.cpp
+++ main.cpp
@@ -4,18 +4,37 @@
 
 int main(void){
     std::vector<int> intVec;
+    // std::vector<int> intVec2;
+    // intVec.reserve(2);
     intVec.push_back(1);
     intVec.push_back(2);
     intVec.push_back(3);
     intVec.push_back(4);
     intVec.push_back(5);
-    // std::cout << intVec.capacity() << std::endl;
-    // std::cout << intVec.size() << std::endl;
-    std::vector<int>::iterator it1;
-    std::vector<int>::iterator it2;
-    std::vector<int>::iterator it3;
-    it1 = intVec.begin() + 1;
-    it2 = intVec.end() - 1;
-    std::cout << it1[-1] << std::endl;
-    std::is_integral<int>
+    // std::vector<int>::iterator erase2 = intVec.erase(intVec.begin() + 1, intVec.end() - 1);
+    // std::cout << *(erase2) << std::endl;
+    std::cout << "diff : " << (intVec.end() - 1) - (intVec.begin() + 1) << std::endl;
+    // intVec.push_back(6);
+    // std::cout << "diff : " << intVec.end() - (intVec.begin() + 2) - 1 << std::endl;
+    // std::vector<int>::iterator erase = intVec.erase(intVec.begin() + 2);
+    // std::cout << "erase = " << *(erase) << std::endl;
+    // std::vector<int>::iterator insert = intVec.insert(intVec.begin()+ 1, 1, 2);
+    // std::cout << "insert : " << *(insert) << std::endl;
+    // intVec2.assign(intVec.begin(), intVec.end());
+    // intVec.push_back(0);
+    // intVec.push_back(0);
+    std::cout << "size = " << intVec.size() << std::endl;
+    std::cout << "capacity = " << intVec.capacity() << std::endl;
+    // std::cout << "front = " << intVec.front() << std::endl;
+    // std::cout << "back = " << intVec.back() << std::endl;
+    // intVec.resize(3, 2);
+    // std::cout << "size = " << intVec.size() << std::endl;
+    // std::cout << "capacity = " << intVec.capacity() << std::endl;
+    std::vector<int>::iterator it = intVec.begin();
+
+    for (; it != intVec.end(); it++){
+        std::cout << *(it) << std::endl;
+    }
+    // std::cout << *(intVec.end()) << std::endl;
+
 }
