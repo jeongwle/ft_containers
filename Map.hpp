78526dfdf1d57de5de3ae148a6d7d729a6163d63
Jeongwook Lee <jeongwle@c5r2s3.42seoul.kr>
2022-02-10 15:45:14 +0900

    [New] map member function 틀만 작성해놓음

--- Map.hpp
+++ Map.hpp
@@ -6,14 +6,15 @@
 /*   By: jeongwle <jeongwle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 09:26:38 by jeongwle          #+#    #+#             */
-/*   Updated: 2022/02/10 11:19:37 by jeongwle         ###   ########.fr       */
+/*   Updated: 2022/02/10 15:24:59 by jeongwle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_HPP
 #define MAP_HPP
 #include <functional>
-#include "MapUtils.hpp"
+#include "RBTree.hpp"
+#include <map>
 
 namespace ft {
     template <class Key,
@@ -40,6 +41,168 @@ namespace ft {
         // typedef const_reverse_iterator;
         // typedef difference_type;
         typedef size_t                                      size_type;
+
+    /* 현재 여기 아래 구현한 거 한개도 없음 그냥 틀 잡아놓은 것 */
+    private :
+        /* private variables */
+
+    public :
+        /* Constructor, Destructor */
+        explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()){
+            /* empty container constructor (default) */
+        }
+
+        template <class InputIterator>
+        map(InputIterator first, InputIterator last,
+        const key_compare& comp = key_compare(),
+        const allocator_type& alloc = allocator_type()){
+            /* range constructor */
+
+        }
+
+        map(const map& x){
+            /* copy constructor */
+        }
+
+        ~map(void){
+
+        }
+
+    public :
+        /* Memeber Function */
+        map& operator=(const map& x){
+
+        }
+
+        iterator begin(void){
+
+        }
+
+        const_iterator begin(void) const{
+
+        }
+
+        iterator end(void){
+
+        }
+
+        const_iterator end(void) const{
+
+        }
+
+        reverse_iterator rbegin(void){
+
+        }
+
+        const_reverse_iterator rbegin(void) const{
+
+        }
+
+        reverse_iterator rend(void){
+
+        }
+
+        const_reverse_iterator rend(void) const{
+
+        }
+
+        bool empty(void) const{
+
+        }
+
+        size_type size(void) const{
+
+        }
+
+        size_type max_size(void) const{
+
+        }
+
+        mapped_type& operator[](const key_type& k){
+
+        }
+
+        pair<iterator, bool> insert(const value_type& val){
+            
+        }
+
+        iterator insert(iterator position, const value_type& val){
+
+        }
+
+        template <class InputIterator>
+        void insert(InputIterator first, InputIterator last){
+
+        }
+
+        void erase(iterator position){
+
+        }
+
+        size_type erase(const key_type& k){
+
+        }
+
+        void erase(iterator first, iterator last){
+
+        }
+
+        void swap(map& x){
+
+        }
+
+        void clear(void){
+
+        }
+
+        key_compare key_comp(void) const{
+
+        }
+
+        value_compare value_comp(void) const{
+
+        }
+
+        iterator find(const key_type& k){
+
+        }
+
+        const_iterator find(const key_type& k) const{
+
+        }
+
+        size_type count(const key_type& k) const{
+
+        }
+
+        iterator lower_bound(const key_type& k){
+
+        }
+
+        const_iterator lower_bound(const key_type& k) const{
+
+        }
+
+        iterator upper_bound(const key_type& k){
+
+        }
+
+        const_iterator upper_bound(const key_type& k) const{
+
+        }
+
+        pair<const_iterator, const_iterator> equal_range(const key_type& k) const{
+
+        }
+
+        pair<iterator, iterator> equal_range(const key_type& k){
+
+        }
+
+        allocator_type get_allocator(void) const{
+            
+        }
+
     };
 
 }
